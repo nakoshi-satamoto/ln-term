2a5b4a77bfec3e7a2fe92da0cc0ced56221b7a04
antifiat, cypherpunk, destroyer of central banks <161100720+nakoshi-satamoto@users.noreply.github.com>
2024-04-10 14:30:56 +0100

    Update ln-term.sh
    New version, cleaned up the code after the inaccurate channel state issue was resolved with the updated command

--- ln-term.sh
+++ ln-term.sh
@@ -2,8 +2,8 @@
 
 # Name: LN-Term
 # Author: The operator of the lightning node nakoshi-satamoto. 
-# Contributers: https://github.com/Lagrang3/ (update-after-deprecated-listpeers)
-# Version timestamp: Block 838463
+# Contributers: https://github.com/Lagrang3/ (updated deprecated listpeers command to listpeerchannels)
+# Version timestamp: Block 838478
 
 # Description: This is intended for use with core lightning. Execute with command "./ln-term.sh" or "watch ./ln-term.sh" for realtime monitoring. This tool shows realtime information of a lightning router including channels, channel balances, and statuses. This was inspired by the core lightning summary.py plugin, but I wanted something that was not using python having no or minimal dependencies. Python apps are a horrendous dependency nightmare with a known high risk of software supply chain compromise. Simplicity is elegance.
 
@@ -11,12 +11,9 @@
 
 # Warning: Use this at your own risk, I accept no responsibility for whatever happens as a result of using or relying on this script. This script is in beta and still needs to be security audited.
 
-# If you see the message "⚠INACCURATE_STATE⚠", read the section below "Note on inaccurate channel states"
-
 #To do list, and features to implement after testing
-#1. Have a way to show the latest channel with a peer instead of duplicate and out of date info. Eliminating the need to have an "INACCURATE_STATE" label in these scenarios.
-#2. Audit security, specifically resilience from bash and jq injections. How well does jq sanitize input?
-#3. Show onchain balance
+#1. Audit security, specifically resilience from bash and jq injections. How well does jq sanitize input?
+#2. Show onchain balance
 
 clear
 date
@@ -47,12 +44,12 @@ echo -n "
 ./lightning-cli getinfo | jq '"alias:" + .alias + " | " + "ID:" + .id'
 echo "--------------------------------------------------------------------------------------------------------------"
 echo -n "Total redeemable sats in chans:           "
-./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.to_us_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo " | If you see ⚠INACCURATE_STATE⚠ for any of the chans,"
+./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.to_us_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo -n " | "
+./lightning-cli getinfo | grep fees | sed -e 's/   "fees_collected_msat":/ fees collected:/g' | tr -d ','  | tr -d '\n' && echo " msats"
 echo -n "Total spendable (out) sats in open chans: "
-./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.spendable_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo " | these totals should be considered inaccurate also."
+./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.spendable_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo " | "
 echo -n "Total receivable (in) sats in open chans: "
-./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.receivable_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo " | Read the documentation in this script for more info."
-./lightning-cli getinfo | grep fees
+./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | (.receivable_msat / 1000)' | jq -s 'add' | awk -F "." {'print $1'} | tr -d '\n' && echo -n " ≐" && echo " | "
 
 #The numbers are made in a range of 11 character space to account for all msats below 1 bitcoin.
 #If you have 1 or more bitcoin (100,000,000,000 msat) in a channel, you can increase the number 11 from below to work with balances above 99,999,999,999 msats.
@@ -60,14 +57,12 @@ echo "                                                                       our
 ./lightning-cli getinfo | jq '"                               Peers:" + (.num_peers | tostring | (length | if . >=4 then "" else "*" * (4 - .) end) as $padding | "\($padding)\(.)") + "  |  Active channels:" + (.num_active_channels | tostring | (length | if . >=4 then "" else "*" * (4 - .) end) as $padding | "\($padding)\(.)") + " |   out(msats) <----> in(msats)    | redeemable  | short chan ID | chan status"'
 echo "____________________________________________________________________|__________________________________|  (msats)"
 
-#The 'sort' command at the end is to list the channels in a uniform and reproducible order by order of public key.
+#The 'sort' command at the end is to list the channels in a uniform and reproducible order by order of public key rather than label.
 #
 #You can optionally assign labels in place of public keys by adding to the 'public key to label' directory in this script below. There are a few added for example purposes. Uncomment the #\ if adding a new line below it. It is recommended to pad the entries with .... to keep the output of ln-term uniform and lined up with the other output. I do not yet know of a suitable way to automatically populate the aliases from the nodes themselves, so this is primarily why it is done manually in this script. Adding the labels manually instead of pulling info from the node's provided alias field might also help with protection from malicious jq injections through the alias fields set by others.
 #
-#Note on inaccurate channel states
-#The 'awk' and 'seen' command is to eliminate duplicates which would occur in certain cases. If you have recently closed a chan with a peer, core lightning remembers that chan for around half a day, 100 blocks or more. If you close a chan with a peer then re-open a new chan to that same peer during this time, jq sees duplicate conflicting states. So instead of showing the duplicate entries and incorrect information this script shows "⚠INACCURATE_STATE⚠". The inaccurate state should go away once Core Lightning forgets the old channel. You can use the command "lightning-cli listpeers" to see how many more blocks until Core Lightning forgets the old channel.
 echo "                                                                      ₿≐≐≐≐≐≐≐≐mmm       ₿≐≐≐≐≐≐≐≐mmm  |"
-./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | .peer_id + " | " + "█" + (.spendable_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + "█" + "<-⚡->" + "█" + (.receivable_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + "█" + " | " + (.to_us_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + " | " + .short_channel_id + " | " + .state + " " + (.peer_connected | tostring)'  | awk -F " | " '!seen[$1]++' | sed -e 's/ONCHAIN/⚠INACCURATE_STATE⚠/g' | sort | sed -e 's/ true/ connected/g' | sed -e 's/ false/ ⚠DISCONNECTED⚠/g' \
+./lightning-cli listpeerchannels | jq '.[] | .[] | select(last(.state_changes[].new_state) != "ONCHAIN") | .peer_id + " | " + "█" + (.spendable_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + "█" + "<-⚡->" + "█" + (.receivable_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + "█" + " | " + (.to_us_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)") + " | " + .short_channel_id + " | " + .state + " " + (.peer_connected | tostring)'  | sort | sed -e 's/ true/ connected/g' | sed -e 's/ false/ ⚠DISCONNECTED⚠/g' \
 | sed \
 -e 's/038e5677dc7d1ec4e9a82bd004d524b2469fedb0867e85bd1e2f5d3352957e03b7/..................................................nakoshi-satamoto/g' \
 -e 's/020a128145c54dec1c1d72e9bf0ec26759d1e16b9a7bba1453e1fc2b4cec6a6fbd/..........................................donate.sparrowwallet.com/g' \
@@ -80,4 +75,3 @@ echo "__________________________________________________________________________
 
 #padding operation. This is the method used in jq to pad symbols or spaces in front of numbers to have number strings of varying length uniform in appearence.
 #(.channels[].receivable_msat | tostring | (length | if . >=11 then "" else "*" * (11 - .) end) as $padding | "\($padding)\(.)")
-
